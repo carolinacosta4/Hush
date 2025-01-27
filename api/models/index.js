4021cddd7dfa8c8cf76f4394ef2e122997d07781
Carolina Costa <carolinamccosta@gmail.com>
2025-01-27 15:38:16 +0000

    organization fixes and token working

--- api/models/index.js
+++ api/models/index.js
@@ -1,22 +1,12 @@
 import mongoose from "mongoose";
 import dbConfig from "../config/db.config.js";
-import User from "./users.model.js";
-import SleepLog from "./sleeplogs.model.js";
 
-const db = {};
-db.mongoose = mongoose;
-db.User = User;
-db.SleepLog = SleepLog;
-
-(async () => {
+export const connectDB = async () => {
   try {
-    console.log(dbConfig.URL);
-    await db.mongoose.connect(dbConfig.URL);
+    await mongoose.connect(dbConfig.URL);
     console.log("Connected to the database!");
   } catch (error) {
     console.log("Cannot connect to the database!", error);
-    process.exit();
+    process.exit(1);
   }
-})();
-
-export default db;
+};
